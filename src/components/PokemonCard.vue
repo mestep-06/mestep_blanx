9d978689116745edaa6d3bd92873faf49e8a08b6
mestep-06 <mestep@insdanielblanxart.cat>
2025-04-11 16:48:46 +0200

    Projecte Finalitzat

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div 
+    class="pokemon-card" 
+    :class="{ selected: isSelected }" 
+    @click="onToggleSelection"
+  >
+    <img :src="pokemon.image" :alt="pokemon.name" class="pokemon-image" />
+    <h3>{{ pokemon.name }}</h3>
+    <p v-if="pokemon.types && pokemon.types.length">
+      Tipus: {{ pokemon.types.join(', ') }}
+    </p>
+    <p>Punts: {{ pokemon.points }}</p>
+    <p v-if="pokemon.special_power">Especial: {{ pokemon.special_power }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokemonCard',
+  props: {
+    // The Pokémon data
+    pokemon: {
+      type: Object,
+      required: true
+    },
+    // Whether this Pokémon is in the team or not (used to set 'selected' class)
+    isSelected: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onToggleSelection() {
+      // Emitir el evento para que el componente padre lo gestione
+      this.$emit('toggle-selection', this.pokemon);
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './assets/estil.css';  // Asegúrate de que estil.css esté en la ruta correcta
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
